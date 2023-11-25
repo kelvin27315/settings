b28bf548551fa1d6440f62c9c02dea4a955db393
kelvin27315 <seven_units@yahoo.co.jp>
2023-11-25 22:56:12 +0900

    fix

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require "wezterm"
 local config = {}
 
 config.color_scheme = "SeaShells"
-config.font = wezterm.font_with_fallback(
+config.font = wezterm.font_with_fallback({
   "Monaspace Neon",
   "Symbols Nerd Font Mono",
   "PlemolJP Console NF",
   "Noto Color Emoji"
-)
+})
 
 return config
